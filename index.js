e29f29f23331ec98718c6af7fb0f3c526625206c
DavidLancheros05 <david.dlls05@gmail.com>
2025-05-27 22:22:55 -0500

    Agrega persistencia con base de datos

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('✅ Conectado a MongoDB'))
+  .catch((err) => console.error('❌ Error conectando a MongoDB:', err));
 require('dotenv').config(); // para cargar variables de entorno
 
 const app = express();
